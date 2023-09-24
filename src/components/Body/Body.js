cf8a3e20da8145daacb91da0466d14d2a5da5fa7
Amayuru Amarasinghe <116435706+Amayuru1999@users.noreply.github.com>
2023-09-24 14:52:02 +0530

    thiird commit

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,16 @@
 import React from "react";
 import backgroundImage from "../../assets/1.jpg";
+import "./Body.css"; // Import your CSS for styling
 
 const Body = () => {
   return (
-    <div>
-      <div className="background">
-        <img src={backgroundImage} alt="Background" />
+    <div className="background">
+      <img src={backgroundImage} alt="Background" />
+      <div className="overlay">
+        <div className="card">
+          <div className="top"></div>
+          <div className="bottom"></div>
+        </div>
       </div>
     </div>
   );
