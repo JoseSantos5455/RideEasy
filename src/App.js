71ba2b6373da1ceb530e95f41346266c74fe4e08
Amayuru Amarasinghe <116435706+Amayuru1999@users.noreply.github.com>
2023-09-24 14:59:12 +0530

    Fourth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Body from "./components/Body/Body";
+import FourRides from "./components/FourRides/FourRides";
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
       <Body />
+      <FourRides />
     </div>
   );
 };
---
+++ src/components/FourRides/FourRides.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FourRides = () => {
+  return <div>Amayuru</div>;
+};
+
+export default FourRides;
