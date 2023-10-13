08d369e22dfac35566a23149b2d4c2de918fa497
Amayuru Amarasinghe <116435706+Amayuru1999@users.noreply.github.com>
2023-10-13 09:51:55 +0530

    fifth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Body from "./components/Body/Body";
 import FourRides from "./components/FourRides/FourRides";
 import Footer from "./components/Footer/Footer";
+import Signup from "./components/Signup/Signup";
 
 const App = () => {
   return (
-    <div className="App">
-      <NavBar />
-      <Body />
-      <FourRides />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/four-rides" element={<FourRides />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.css";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here (e.g., send data to the server)
+    console.log("Login submitted:", formData);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
